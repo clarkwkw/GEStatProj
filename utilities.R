9af2073ce1e50ed6bae776278af9a5960b53aad5
clarkwong <clarkwkw@yahoo.com.hk>
2016-05-23 18:53:48 +0800

    Combined with Owen's code
    Added generateRecordsByTeacher(), which generates data frames for each
    teacher
    Added comments
    Fixed coding style

--- utilities.R
+++ utilities.R
@@ -1,19 +1,73 @@
-#This func is to merge two dataframes containing two columns: "Tag Number" & "Reason"
+#This func. is to merge two dataframes containing two columns: "Tag Number" & "Reason"
 mergeInvalidDF <- function(a, b){
   merge(a, b, by = c("Tag Number", "Reason"), all=TRUE)
 }
 
-#check data in data frame in a specified column (colname)
+#check data in a specified column (colname) of dataframe (df)
 #compare it with pre-specified range of values (rng, a list)
-#return a data frame of invalid records with tag number and colname
+#return a data frame of invalid records with tag number and column name
 checkdata <- function(df, rng, colname){
+  #record N/A value of rng seperately
   isNAValue = any(rng == "N/A", na.rm = TRUE)
+  
+  #remove blank/"N/A"/NA values of rng
   rng <- rng[rng != "" & rng != "N/A" & !is.na(rng)]
-  if(length(levels(rng)))rng <- levels(droplevels(rng))
-  else rng <- c(rng)
-  if (isNAValue) rng <- c(rng, NA)
-  tmpdf <- df[which(names(df) %in% c(colname, "Tag Number"))]
+  
+  #try to vectorize rng, if it is factor, use method 1. Otherwise, method 2
+  if(length(levels(rng)))
+    rng <- levels(droplevels(rng)) # method 1
+  else
+    rng <- c(rng) #method 2
+  
+  #try to convert rng vector to numeric if possible
+  if(all(suppressWarnings(!is.na(as.numeric(rng))))) 
+    rng<-as.numeric(rng)
+  
+  #add the NA value back to the rng vector if necessary
+  if (isNAValue) 
+    rng <- c(rng, NA)
+  
+  #extract related columns from df
+  tmpdf <- df[which(names(df) %in% c(colname, "Tag Number"))] 
+  
+  #If the values in the specific column is not one of the values specified in rng, the record is invalid
+  #Tag Numbers of invalid records are extracted
   tmpdf <- tmpdf[(tmpdf[[colname]] %in% rng) == FALSE, "Tag Number", drop = FALSE]
-  if(nrow(tmpdf))tmpdf[["Reason"]] = colname
+  
+  #If no. of invalid records > 0, set their reasons to colname (specifying why they are invalid)
+  if(nrow(tmpdf)) 
+    tmpdf[["Reason"]] = colname
+  
+  #Set invalid cells in rawdata to NA
+  df[(df[[colname]] %in% rng) == FALSE,colname] <- NA
+  rawdata<<-df
+  
   return (tmpdf)
+}
+
+#Example: generateRecordsByTeacher(rawdata, "df", "") will generate those dataframes:
+#df_Amber, df_Andy, df_Balwin, ...
+#while generateRecordsByTeacher(rawdata, "", "data") will generate
+#Amber_data, Andy_data, Balwin_data, ...
+generateRecordsByTeacher <- function(df, dfPrefix, dfSuffix){
+  #Split the orginal dataframe according to teachers' names, dfs will be a list of dataframes
+  dfs <- split(df, f = df$"Name")
+  
+  #For each dataframe in the dfs list, assign it to a global variable, whose name is dfPrefix_(teacher name)
+  #Invisible() is for hiding the return value from appearing in console
+  invisible(lapply(dfs, function(x){
+    #initialize dfname to the teacher's name
+      dfname<-x[1, "Name"]
+      
+      #if dfPrefix is not an empty string, append it to dfname
+      if(dfPrefix != "")
+        dfname<-paste(dfPrefix, dfname, sep="_")
+      
+      #if dfSuffix is not an empty string, append it to dfname
+      if(dfSuffix != "")
+        dfname<-paste(dfname, dfSuffix, sep="_")
+      
+      #assign dataframe as a dataframe global variable whose name is dfname
+      assign(dfname, x, envir = .GlobalEnv)
+  }))
 }
