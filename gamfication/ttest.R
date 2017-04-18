f72fe0209ffee3300f64757d7a13cb7afd274a87
Poon Ling <poonling456@yahoo.com.hk>
2017-04-18 15:10:01 +0800

    better output naming

--- gamfication/ttest.R
+++ gamfication/ttest.R
@@ -1,4 +1,4 @@
-#install.packages("reshape")
+install.packages("reshape")
 library(reshape)
 
 data <- read.csv("data.csv", header = TRUE, sep = ",",row.names = 1)
@@ -21,12 +21,12 @@ for(i in 1:(ncol(group)-1))
 testResult=t(apply(comb[,c(-83,groupLabel)],2, function(x) unlist(t.test(x~comb$Year))))
 write.csv(testResult,file="Year.csv")
 
-#t test of bad DSE Eng. student grouped by civ 4 player or not
+#t test of lower DSE Eng. student grouped by civ 4 player or not
 badEng=subset(comb, Eng=="DSE 4 or below")
 testResult=t(apply(badEng[,groupLabel],2, function(x) unlist(t.test(x~badEng$Civ4))))
-write.csv(testResult,file="Bad_Eng_DSE.csv")
+write.csv(testResult,file="LowerEng_civ4player.csv")
 
 #t test of civ 4 player grouped by english proficiency
 civPlayer=subset(comb, Civ4==1)
 testResult=t(apply(civPlayer[,groupLabel],2, function(x) unlist(t.test(x~civPlayer$Eng))))
-write.csv(testResult,file="Lower_Eng_DSE.csv")
+write.csv(testResult,file="civ4player_Eng.csv")
